20cdbabeff544524eb8dc651f9abab5e6d504a4c
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 15:06:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 def line(katz_deli)
+  customerLine = []
   if katz_deli.length > 0
     katz_deli.each.with_index do |name, index|
-      formatLine.push("#{index}. #{name}")
+      customerLine.push("#{index}. #{name}")
     end
     print "The line is currently: #{formatLine.join(' ')}"
   else

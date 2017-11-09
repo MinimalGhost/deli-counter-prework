15f66e1559dce7cc15b17c90de19bc9cb96a3d9d
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 15:03:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(katz_deli)
   if katz_deli.length > 0
     katz_deli.each.with_index do |customer, index|
-      puts customer
+      formatLine.push("#{index}. #{customer}")
     end
   else
     puts "The line is currently empty."

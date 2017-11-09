d219e92e1eb23a3c59df6347f5404009ec70ff8d
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 15:17:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome #{name}. You are number #{katz_deli(name).to_find} "
+  puts "Welcome #{name}. You are number #{katz_deli.index} in line."
 end

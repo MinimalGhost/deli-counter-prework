fc9fe9a3065f06c4668471b96e638de216c85087
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 15:07:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
     katz_deli.each.with_index do |customer, index|
       customerLine.push("#{index}. #{customer}")
     end
-    print "The line is currently: #{customerLine.join(' ')}"
+    puts "The line is currently: #{customerLine.join(' ')}"
   else
     puts "The line is currently empty."
   end

17422a62152ef1315ba85f7d35bdcc512b56d35d
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 15:05:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 def line(katz_deli)
   if katz_deli.length > 0
     katz_deli.each.with_index do |name, index|
-      formatLine.push("#{index}. #{name} ")
+      formatLine.push("#{index}. #{name}")
     end
-    print "The line is currently: #{formatLine.join}"
+    print "The line is currently: #{formatLine.join(' ')}"
   else
     puts "The line is currently empty."
   end
